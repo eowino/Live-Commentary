43037a6f163a58e66521204550e1bd4308775660
Evans Owino <evansowino@hotmail.com>
2018-12-12 22:58:36 +0000

    ordered time

--- src/api/commentaries.ts
+++ src/api/commentaries.ts
@@ -25,14 +25,14 @@ const commetaries: ICommentary[] = [
       "AND THERE IS THE GOAL! No mistake from Kane, as - after initially missing Son's cross - he gets back up off the turf to cushion in Trippier's pinpoint cut-back.",
     id: '4',
     time: '21',
-    highlight: 'Goal',
+    highlight: 'Goal ⚽️',
   },
   {
     body:
       'SAVE! Sanchez curls a free-kick towards the left-hand corner, but Lloris reacts quickly to jump to his right and make the stop.',
     id: '5',
     time: '28',
-    highlight: 'Save',
+    highlight: 'Save 🧤',
   },
   {
     body:
@@ -49,131 +49,113 @@ const commetaries: ICommentary[] = [
   {
     body: 'Laboris cupidatat ad consectetur ullamco dolor ex deserunt.',
     id: '5c1101e12c912945b8f7ab04',
-    time: '25',
-    highlight: 'Action Here',
+    time: '33',
   },
   {
     body: 'Minim esse ex ex commodo irure officia.',
     id: '5c1101e1790dad0397601a77',
-    time: '28',
-    highlight: 'Action Here',
+    time: '39',
   },
   {
     body:
       'Velit voluptate aute sunt id ea fugiat eu aliqua mollit ullamco duis incididunt.',
     id: '5c1101e10de0a4f97ea8cb7b',
-    time: '31',
-    highlight: 'Action Here',
+    time: '49',
   },
   {
     body: 'Amet aliquip deserunt in ut consequat reprehenderit duis ex.',
     id: '5c1101e136171c69ef9cc7f2',
-    time: '24',
-    highlight: 'Action Here',
+    time: '50',
   },
   {
     body:
       'Fugiat culpa eiusmod qui Lorem velit amet incididunt elit aliquip magna quis commodo.',
     id: '5c1101e1c1e2bf64a65d5864',
-    time: '21',
-    highlight: 'Action Here',
+    time: '55',
   },
   {
-    body:
-      'Aliquip do ipsum tempor aliqua dolore velit Lorem do magna cupidatat.',
+    body: 'Big chance missed by Kane! He fires the shot just wide',
     id: '5c1101e13b71cb74cb6785e6',
-    time: '26',
-    highlight: 'Action Here',
+    time: '59',
+    highlight: 'Missed chance by Spurs',
   },
   {
     body:
       'Ullamco eiusmod voluptate quis ut officia laborum veniam excepteur ad.',
     id: '5c1101e149c3e8bc384702d3',
-    time: '15',
-    highlight: 'Action Here',
+    time: '63',
   },
   {
     body: 'Adipisicing anim est non eu nisi quis laborum nostrud non enim.',
     id: '5c1101e1a0977f3614e8141d',
-    time: '41',
-    highlight: 'Action Here',
+    time: '65',
   },
   {
     body: 'Aliqua aliqua ut consequat sit qui ipsum non labore.',
     id: '5c1101e12cccccc4baf4e97f',
-    time: '64',
-    highlight: 'Action Here',
+    time: '68',
   },
   {
     body: 'Laborum aliquip excepteur veniam dolor aliquip.',
     id: '5c1101e17df21802947d9326',
-    time: '2',
-    highlight: 'Action Here',
+    time: '71',
   },
   {
     body: 'Reprehenderit nulla dolor non cupidatat incididunt Lorem aliquip.',
     id: '5c1101e18b053c470fef1fe5',
-    time: '62',
-    highlight: 'Action Here',
+    time: '73',
   },
   {
     body:
       'Elit aliqua sint anim irure excepteur exercitation aute minim nulla dolor cupidatat culpa.',
     id: '5c1101e1321221a5d70233a3',
-    time: '71',
-    highlight: 'Action Here',
+    time: '75',
   },
   {
     body:
       'Officia amet fugiat ut culpa enim enim sint minim in sit adipisicing labore commodo.',
     id: '5c1101e133a08cd971a5bdc7',
-    time: '2',
-    highlight: 'Action Here',
+    time: '77',
   },
   {
-    body: 'Adipisicing esse dolor laboris anim sint cupidatat magna est eu.',
+    body:
+      'GOAL! Wonderful. Wonderful. Wonderful. What an amazing volley from Martial to equalize for United. Game on!',
     id: '5c1101e12f12f2a999f0715d',
     time: '84',
-    highlight: 'Action Here',
+    highlight: 'Goal ⚽️',
   },
   {
     body:
       'Labore mollit in eu pariatur nostrud irure sint eu veniam tempor labore pariatur.',
     id: '5c1101e1cbeabdccb8a900a4',
-    time: '18',
-    highlight: 'Action Here',
+    time: '85',
   },
   {
     body: 'Commodo id incididunt dolor aliquip magna.',
     id: '5c1101e1f646ddd059c39888',
-    time: '36',
-    highlight: 'Action Here',
+    time: '87',
   },
   {
     body:
       'Pariatur et occaecat ad officia sunt ad eu aliqua ullamco labore aliqua non.',
     id: '5c1101e14dc7fe7a4597a2da',
-    time: '70',
-    highlight: 'Action Here',
+    time: '90',
   },
   {
     body: 'Sit magna laborum cillum quis ipsum amet est anim velit.',
     id: '5c1101e15a26c89fe0c4b1e0',
-    time: '19',
-    highlight: 'Action Here',
+    time: '90+1',
   },
   {
     body:
       'Nulla dolor duis proident in cillum occaecat id adipisicing do proident ad reprehenderit.',
     id: '5c1101e1a3ee67b1f751c10b',
-    time: '77',
-    highlight: 'Action Here',
+    time: '90+2',
   },
   {
     body: 'Velit ea eiusmod minim cupidatat reprehenderit ipsum quis.',
     id: '5c1101e18c5184bd107fd543',
-    time: '51',
-    highlight: 'Action Here',
+    time: '90+3',
   },
 ];
 
